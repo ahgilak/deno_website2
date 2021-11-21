317a045864f6716eb21e30d2e55370b0b0b9d287
Ahmadreza Gilak <ah.gilak@outlook.com>
2021-11-21 13:22:01 +0330

    translate meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,30 +20,30 @@ export default class DenoDocDocument extends Document {
 
   render(): React.ReactElement {
     return (
-      <Html lang="en">
+      <Html lang="fa">
         <Head>
           <meta name="twitter:site" content="@deno_land" />
           <meta name="twitter:creator" content="@deno_land" />
           <meta
             name="twitter:title"
-            content="Deno - A modern runtime for JavaScript and TypeScript"
+            content="دینو - محیطی مدرن برای اجرای جاوااسکریپت و تایپ‌اسکریپت"
           />
           <meta
             name="twitter:description"
-            content="Deno is a simple, modern runtime for JavaScript and
-            TypeScript that uses V8 and is built in Rust."
+            content="دینو یک محیط اجرایی ساده و مدرن برای جاوااسکریپت و
+            تایپ‌اسکریپت است که با استفاده از V8 به زبان Rust نوشته شده"
           />
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="og:locale" content="en_US" />
           <meta property="og:site_name" content="DenoLand" />
           <meta
             property="og:title"
-            content="Deno - A modern runtime for JavaScript and TypeScript"
+            content="دینو - محیطی مدرن برای اجرای جاوااسکریپت و تایپ‌اسکریپت"
           />
           <meta
             property="og:description"
-            content="Deno is a simple, modern runtime for JavaScript and
-            TypeScript that uses V8 and is built in Rust."
+            content="دینو یک محیط اجرایی ساده و مدرن برای جاوااسکریپت و
+            تایپ‌اسکریپت است که با استفاده از V8 به زبان Rust نوشته شده"
           />
           <meta property="og:image" content="/images/icons/icon-512x512.png" />
           <meta property="og:type" content="website" />
