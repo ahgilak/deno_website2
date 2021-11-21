060a9c396c4f37c0f2754f77f1f6405fc2fd661c
Ahmadreza Gilak <ah.gilak@outlook.com>
2021-11-21 19:28:29 +0330

    translate main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
   return (
     <>
       <Head>
-        <title>Deno - A modern runtime for JavaScript and TypeScript</title>
+        <title>دینو - محیطی مدرن برای اجرای جاوااسکریپت و تایپ‌اسکریپت</title>
       </Head>
       <CookieBanner />
       {
@@ -41,24 +41,24 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
         </div>
       </div> */
       }
-      <div className="bg-white">
+      <div className="bg-white" dir="rtl">
         <div className="bg-gray-50 border-b border-gray-200">
           <Header main />
           <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col items-center">
             <h1 className="font-extrabold text-5xl leading-10 tracking-tight text-gray-900">
-              Deno
+              دینو
             </h1>
             <h2 className="mt-4 sm:mt-5 font-light text-2xl text-center leading-tight text-gray-900">
-              A <strong className="font-semibold">modern</strong> runtime for
+              محیطی <strong className="font-semibold">مدرن</strong> برای اجرای
               {" "}
-              <strong className="font-semibold">JavaScript</strong> and{" "}
-              <strong className="font-semibold">TypeScript</strong>.
+              <strong className="font-semibold">جاوااسکریپت</strong> و{" "}
+              <strong className="font-semibold">تایپ‌اسکریپت</strong>.
             </h2>
             <a
               href="/#installation"
               className="rounded-full mt-8 px-8 py-2 transition-colors duration-75 ease-in-out bg-blue-500 hover:bg-blue-400 text-white text-lg shadow-lg"
             >
-              Install
+              نصب
             </a>
             <a
               href="https://github.com/denoland/deno/releases/latest"
@@ -70,35 +70,35 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <p className="my-4 text-gray-700">
-            Deno is a simple, modern and secure runtime for JavaScript and
-            TypeScript that uses V8 and is built in Rust.
+            دینو یک محیط اجرایی ساده و مدرن برای جاوااسکریپت و تایپ‌اسکریپت است
+            که با استفاده از V8 به زبان Rust نوشته شده.
           </p>
           <ol className="ml-8 list-disc text-gray-700">
             <li>
-              Secure by default. No file, network, or environment access, unless
-              explicitly enabled.
+              ایمن به صورت پیش‌فرض،‌ امکان تنظیم دسترسی به فایل‌ها، شبکه و محیط
+              اجرا.
             </li>
-            <li>Supports TypeScript out of the box.</li>
-            <li>Ships only a single executable file.</li>
+            <li>پشتیبانی از تایپ اسکریپت.</li>
+            <li>امکان کامپایل به یک فایل اجرایی.</li>
             <li>
-              Has built-in utilities like a dependency inspector (
-              <InlineCode>deno info</InlineCode>) and a code formatter (
+              دارای ابزار های داخلی مانند کنترل‌ کننده‌ی وابستگی‌ها (
+              <InlineCode>deno info</InlineCode>) و فرمت‌گر کد (
               <InlineCode>deno fmt</InlineCode>).
             </li>
             <li>
-              Has a set of reviewed (audited) standard modules that are
-              guaranteed to work with Deno:{" "}
+              دارای مجموعه‌ای از ماژول‌های استاندارد که تضمین شده‌اند به خوبی با
+              دینو کار کنند:{" "}
               <a href="https://deno.land/std" className="link">
                 deno.land/std
               </a>
             </li>
             <li>
-              Has a number of{" "}
+              مجموعه‌ای از{" "}
               <a
                 href="https://github.com/denoland/deno/wiki#companies-interested-in-deno"
                 className="link"
               >
-                companies interested in using and exploring Deno
+                شرکت‌ها که به دینو علاقه‌مند‌ند.
               </a>
             </li>
           </ol>
@@ -107,7 +107,7 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
           <Link href="#installation">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="installation">
-                Installation
+                نصب
               </h3>
             </a>
           </Link>
@@ -117,16 +117,16 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
           <Link href="#getting-started">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="getting-started">
-                Getting Started
+                شروع
               </h3>
             </a>
           </Link>
-          <p className="my-4 text-gray-700">Try running a simple program:</p>
+          <p className="my-4 text-gray-700">اجرای یک برنامه‌ی ساده:</p>
           <CodeBlock
             code="deno run https://deno.land/std/examples/welcome.ts"
             language="bash"
           />
-          <p className="my-4 text-gray-700">Or a more complex one:</p>
+          <p className="my-4 text-gray-700">مثال پیچیده‌تر:</p>
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8">
           <CodeBlock
@@ -137,116 +137,116 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8">
           <p className="my-4 text-gray-700">
-            You can find a more in depth introduction, examples, and environment
-            setup guides in{" "}
+            می‌توانید معرفی دقیق‌تر، مثال‌ها و راهنمایی راه‌اندازی محیط را در
+            {" "}
             <Link href="/manual">
-              <a className="link">the manual</a>
-            </Link>
-            .
+              <a className="link">راهنما</a>
+            </Link>{" "}
+            بخوانید.
           </p>
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <Link href="#runtime-documentation">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="runtime-documentation">
-                Runtime Documentation
+                مستندات محیط اجرایی
               </h3>
             </a>
           </Link>
           <p className="my-4 text-gray-700">
-            The basic runtime documentation for Deno can be found on{" "}
+            مستندات ابتدایی را میتوانید در{" "}
             <a href="https://doc.deno.land/builtin/stable" className="link">
               doc.deno.land
-            </a>
-            .
+            </a>{" "}
+            بخوانید.
           </p>
           <p className="my-4 text-gray-700">
-            Deno comes with{" "}
+            دینو دارای یک{"  "}
             <Link href="/manual">
-              <a className="link">a manual</a>
+              <a className="link">راهنما</a>
             </Link>{" "}
-            which contains more in depth explanations about the more complex
-            functions of the runtime, an introduction to the concepts that Deno
-            is built on, details about the internals of Deno, how to embed Deno
-            in your own application and how to extend Deno using Rust plugins.
+            شامل توضیحات عمیق‌تری در مورد عملکردهای پیچیده‌تر زمان اجرا،
+            مقدمه‌ای بر مفاهیمی که دینو بر پایه آن‌ها شکل گرفته، جزئیاتی در مورد
+            بخش‌های داخلی دینو، نحوه استفاده از دینو در اپلیکیشن خود و استفاده
+            از افزونه‌های Rust برای افزایش کاریی دینو است.
           </p>
           <p className="my-4 text-gray-700">
-            The manual also contains information about the built in tools that
-            Deno provides.
+            این راهنما همچنین شامل توضیحاتی در مورد ابزار‌های داخلی دینو است.
           </p>
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <Link href="#standard-modules">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="standard-modules">
-                Standard Modules
+                ماژول‌های استاندارد
               </h3>
             </a>
           </Link>
           <p className="my-4 text-gray-700">
-            Next to the Deno runtime, Deno also provides a list of audited
-            standard modules that are reviewed by the Deno maintainers and are
-            guaranteed to work with a specific Deno version. These live in the
-            {" "}
-            <a href="https://github.com/denoland/deno_std" className="link">
+            در کنار محیط اجرایی، دینو دارای مجموعه‌ای از ماژول‌های استاندارد است
+            توسط تیم دینو توسعه و ارزیابی می‌شوند و تضمین شده‌اند که به خوبی با
+            دینو کار کنند. این ماژول‌ها در{" "}
+            <a href="https://github.com/denoland/denoـstd" className="link">
               denoland/deno_std
             </a>{" "}
-            repository.
+            قابل مشاهده هستند.
           </p>
           <p className="my-4 text-gray-700">
-            These standard modules are hosted at{" "}
+            این ماژول‌های استاندارد در{" "}
             <Link href="/std">
               <a className="link">deno.land/std</a>
             </Link>{" "}
-            and are distributed via URLs like all other ES modules that are
-            compatible with Deno.
+            میزبانی می‌شوند و مانند سایر ماژول‌های اکمااسکریپت که با دینو سازگار
+            هستند از طریق آدرس‌های اینترنتی توزیع می‌شوند.{" "}
           </p>
         </div>
         <div className="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 mt-20">
           <Link href="#third-party-modules">
             <a className="hover:underline">
               <h3 className="font-bold text-xl" id="third-party-modules">
-                Third Party Modules
+                ماژول‌های متفرقه
               </h3>
             </a>
           </Link>
           <p className="my-4 text-gray-700">
-            Deno can import modules from any location on the web, like GitHub, a
-            personal webserver, or a CDN like{" "}
+            شما می‌توانید از هر آدرس اینترنتی مانند گیت‌هاب، سرور شخصی یا CDN
+            هایی مثل{" "}
             <a href="https://www.skypack.dev" className="link">
               Skypack
-            </a>
-            ,{" "}
+            </a>{" "}
+            ،
             <a href="https://jspm.io" className="link">
               jspm.io
-            </a>
-            ,{" "}
+            </a>{" "}
+            ،
             <a href="https://www.jsdelivr.com/" className="link">
               jsDelivr
             </a>{" "}
-            or{" "}
+            یا{" "}
             <a href="https://esm.sh/" className="link">
               esm.sh
-            </a>
-            .
+            </a>{" "}
+            ماژول های خود را وارد کنید.
           </p>
           <p className="my-4 text-gray-700">
-            To make it easier to consume third party modules Deno provides some
-            built in tooling like <InlineCode>deno info</InlineCode> and{" "}
-            <InlineCode>deno doc</InlineCode>. deno.land also provides a web UI
-            for viewing module documentation. It is available at{" "}
+            برای راحت‌تر کردن استفاده از ماژول ها، دینو در داخل خود ابزارهایی
+            مانند
+            <InlineCode>deno info</InlineCode> و{" "}
+            <InlineCode>deno doc</InlineCode>{" "}
+            را ارائه می‌دهد. وب‌سایت دینو یک محیط گرافیکی تحت وب نیز برای مشاهده
+            مستندات ماژول‌ها ارائه می‌دهد که در{"  "}
             <a href="https://doc.deno.land" className="link">
               doc.deno.land
-            </a>
-            .
+            </a>{" "}
+            قابل مشاهده است.
           </p>
           <p className="my-4 text-gray-700">
-            deno.land also provides a simple public hosting service for ES
-            modules that work with Deno. It can be found at{" "}
+            همچنین وب‌سایت دینو دارای یک سرویس عمومی برای میزبانی از ماژول‌های
+            اکمااسکریپت است که از طریق{" "}
             <Link href="/x">
               <a className="link">deno.land/x</a>
-            </Link>
-            .
+            </Link>{" "}
+            در دسترس است.
           </p>
         </div>
         <div className="mt-20">
@@ -260,7 +260,7 @@ serve((req) => new Response("Hello World\\n"), { addr: ":8000" });
 const InstallSection = () => {
   const shell = (
     <div key="shell" className="my-4 text-gray-700">
-      <p className="py-2">Shell (Mac, Linux):</p>
+      <p className="py-2">Shell (مک, لینوکس):</p>
       <CodeBlock
         language="bash"
         code={`curl -fsSL https://deno.land/x/install/install.sh | sh`}
@@ -273,14 +273,14 @@ const InstallSection = () => {
         <a href="https://formulae.brew.sh/formula/deno" className="link">
           Homebrew
         </a>{" "}
-        (Mac):
+        (مک):
       </p>
       <CodeBlock language="bash" code={`brew install deno`} />
     </div>
   );
   const powershell = (
     <div key="powershell" className="my-4 text-gray-700">
-      <p className="mb-2">PowerShell (Windows):</p>
+      <p className="mb-2">PowerShell (ویندوز):</p>
       <CodeBlock
         language="bash"
         code={`iwr https://deno.land/x/install/install.ps1 -useb | iex`}
@@ -293,7 +293,7 @@ const InstallSection = () => {
         <a href="https://chocolatey.org/packages/deno" className="link">
           Chocolatey
         </a>{" "}
-        (Windows):
+        (ویندوز):
       </p>
       <CodeBlock language="bash" code={`choco install deno`} />
     </div>
@@ -304,7 +304,7 @@ const InstallSection = () => {
         <a href="https://scoop.sh/" className="link">
           Scoop
         </a>{" "}
-        (Windows):
+        (ویندوز):
       </p>
       <CodeBlock language="bash" code={`scoop install deno`} />
     </div>
@@ -312,7 +312,7 @@ const InstallSection = () => {
   const cargo = (
     <div key="cargo" className="my-4 text-gray-700">
       <p className="py-2">
-        Build and install from source using{" "}
+        نصب از سورس{" "}
         <a href="https://crates.io/crates/deno" className="link">
           Cargo
         </a>
@@ -325,13 +325,12 @@ const InstallSection = () => {
   return (
     <>
       <p className="my-4 text-gray-700">
-        Deno ships as a single executable with no dependencies. You can install
-        it using the installers below, or download a release binary from the
-        {" "}
+        دینو یک فایل قابل اجرا بدون هیچگونه وابستگی است. می‌توانید از روش های
+        زیر برای نصب استفاده کنید یا به طور مستقیم از{" "}
         <a href="https://github.com/denoland/deno/releases" className="link">
-          releases page
-        </a>
-        .
+          این صفحه
+        </a>{" "}
+        دانلود کنید.
       </p>
       {shell}
       {powershell}
@@ -340,11 +339,11 @@ const InstallSection = () => {
       {scoop}
       {cargo}
       <p className="my-4 text-gray-700">
-        See{" "}
+        برای دیدن روش های بیشتر به{" "}
         <a className="link" href="https://github.com/denoland/deno_install">
-          deno_install
+          نصب دینو
         </a>{" "}
-        for more installation options.
+        .بروید
       </p>
     </>
   );
