949311154da6d67bf170b91b9ebc6fce7c7bbdcf
Ahmadreza Gilak <ah.gilak@outlook.com>
2021-11-21 13:49:52 +0330

    translate header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ function Header({
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="relative py-6 z-10">
+    <div className="relative py-6 z-10" dir="rtl">
       <nav
         className={`mx-auto flex items-center justify-between px-4 sm:px-6 md:px-8 ${
           widerContent ? "max-w-screen-xl" : "max-w-screen-lg lg:p-0"
@@ -29,7 +29,7 @@ function Header({
               {!main &&
                 (
                   <div className="font-bold text-gray-900 leading-tight text-2xl sm:text-3xl tracking-tight">
-                    Deno
+                    دینو
                   </div>
                 )}
               {subtitle &&
@@ -53,7 +53,7 @@ function Header({
               fill="none"
               viewBox="0 0 24 24"
             >
-              <title>Menu | Deno</title>
+              <title>منو | دینو</title>
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -65,51 +65,49 @@ function Header({
         </div>
         <div className="hidden lg:flex md:ml-10 items-end">
           <Link href="https://deno.com/deploy">
-            <a
-              className={`font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out`}
-            >
-              Deploy
+            <a className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+              دیپلوی
             </a>
           </Link>
           <Link href="/manual">
             <a className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
-              Manual
+              راهنما
             </a>
           </Link>
           <a
             href="https://deno.com/blog"
             className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
           >
-            Blog
+            وبلاگ
           </a>
           <a
             href="https://doc.deno.land/builtin/stable"
             className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
           >
-            API
+            مستندات
           </a>
           <Link href="/std">
             <a className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
-              Standard Library
+              کتابخانه استاندارد
             </a>
           </Link>
           <Link href="/x">
-            <a className="ml-10 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
-              Third Party Modules
+            <a className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">
+              ماژول‌های متفرقه
             </a>
           </Link>
           <a
             href="https://github.com/denoland"
-            className="ml-10 text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+            className="mr-10 text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
             style={{ lineHeight: 0 }}
           >
-            <span className="sr-only">GitHub</span>
+            <span className="sr-only">گیت‌هاب</span>
             <svg
               className="h-6 w-6 inline"
               fill="currentColor"
               viewBox="0 0 24 24"
             >
-              <title>Github | Deno</title>
+              <title>گیت‌هاب | دینو</title>
               <path
                 fillRule="evenodd"
                 d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
@@ -140,9 +138,9 @@ function Header({
                       src="/logo.svg"
                       alt=""
                     />
-                    <div className="ml-5 flex flex-col justify-center">
+                    <div className="mr-5 flex flex-col justify-center">
                       <div className="font-bold text-gray-900 leading-tight text-2xl sm:text-3xl tracking-tight">
-                        Deno
+                        دینو
                       </div>
                       {subtitle &&
                         (
@@ -153,7 +151,7 @@ function Header({
                     </div>
                   </a>
                 </Link>{" "}
-                <div className="-mr-2">
+                <div className="-ml-2">
                   <button
                     type="button"
                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
@@ -180,33 +178,33 @@ function Header({
                   <a
                     className={`block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out`}
                   >
-                    Deploy
+                    دیپلوی
                   </a>
                 </Link>
                 <Link href="/manual">
                   <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
-                    Manual
+                    راهنما
                   </a>
                 </Link>
                 <Link href="https://deno.com/blog">
                   <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
-                    Blog
+                    وبلاگ
                   </a>
                 </Link>
                 <a
                   href="https://doc.deno.land/builtin/stable"
                   className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
                 >
-                  API
+                  مستندات
                 </a>
                 <Link href="/std">
                   <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
-                    Standard Library
+                    کتابخانه استاندارد
                   </a>
                 </Link>
                 <Link href="/x">
                   <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
-                    Third Party Modules
+                    ماژول‌های متفرقه
                   </a>
                 </Link>
               </div>
